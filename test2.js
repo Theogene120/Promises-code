40d1bec0ca777286314d13179d54f76b263d7f68
Theogene <igiranezatheogene4@gmail.com>
2026-01-29 16:24:18 +0200

    last changes

--- test2.js
+++ test2.js
@@ -2,25 +2,30 @@
 
 //https://jsonplaceholder.typicode.com/users
 
-function asyncCounter() {
-  const result = [];
+const nums = [1, 2, 3, 4, 5];
 
-  function delay(number) {
-    return new Promise(resolve => {
-      setTimeout(() => {
-        console.log(number);
-        result.push(number);
-        resolve();
-      }, 1000);
-    });
+const promise = (num) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(num);
+    }, 1000);
+  });
+};
+
+async function display(nums) {
+  for (const num of nums) {
+    const dis = await promise(num);
+    console.log(dis);
   }
-
-  return delay(1)
-    .then(() => delay(2))
-    .then(() => delay(3))
-    .then(() => delay(4))
-    .then(() => delay(5))
-    .then(() => result);
 }
 
-asyncCounter().then(arr => console.log("Final:", arr));
+display(nums);
+
+// function display(numbers) {
+//   numbers.forEach(async (num) => {
+//     const dis = await promise(num);
+//     console.log(dis);
+//   });
+// }
+
+// display(nums);
